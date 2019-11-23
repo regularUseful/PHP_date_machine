6df1b44b2a67a82ed41a0b42ecde71bb5b27d973
scatterbrain3 <scattini3@hotmail.com>
2019-11-24 02:33:31 +1030

    Now says what day it is

--- index.php
+++ index.php
@@ -6,6 +6,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
-
+        <?php
+            $dayOfWeek = date("w");
+            switch ($dayOfWeek){
+                case 1:
+                    echo "It is Monday";
+                break;
+                case 2:
+                    echo "It is Tuesday";
+                break;
+                case 3:
+                    echo "It is Wednesday";
+                break;
+                case 4:
+                    echo "It is Thursday";
+                break;
+                case 5:
+                    echo "It is Friday";
+                break;
+                case 6:
+                    echo "It is Saturday";
+                break;
+                case 0:
+                    echo "It is Sunday";
+                break;
+            }
+        ?>
     </body>
 </html>
